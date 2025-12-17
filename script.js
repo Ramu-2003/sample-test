d817a86168b151d0a843c2bfd56ac120987f8f75
G RAMA KRISHNA PRASAD <132869239+Ramu-2003@users.noreply.github.com>
2025-12-17 10:57:27 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById("btn");
-const title = document.getElementById("title");
-
-button.addEventListener("click", () => {
-    title.textContent = "Button Clicked!";
-    title.style.color = "green";
-});
